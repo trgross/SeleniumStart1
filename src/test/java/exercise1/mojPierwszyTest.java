92d456420cc7280c4c0833ed59cb6139c8e24656
trgross <tgross@fis-sst.pl>
2017-11-17 17:02:22 +0100

    Zmiany na koniec kursu

--- src/test/java/exercise1/mojPierwszyTest.java
+++ src/test/java/exercise1/mojPierwszyTest.java
@@ -6,11 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,19 +23,46 @@ public class mojPierwszyTest {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
+    @Parameters({"seleniumHub", "seleniumPort", "browser", "url"})
+    public void setUp(String seleniumHub, int seleniumPort, String browser, String url) throws MalformedURLException {
+        if (browser.equals("firefox")){
+            DesiredCapabilities ffCaps = DesiredCapabilities.firefox();
+            ffCaps.setCapability(CapabilityType.BROWSER_NAME, browser);
+
+            driver = new RemoteWebDriver
+                    (new URL("http://"+seleniumHub+":"+seleniumPort+"/wd/hub"),ffCaps);
+        } else if (browser.equals("chrome")){
+            DesiredCapabilities chromeCaps = DesiredCapabilities.chrome();
+            chromeCaps.setCapability(CapabilityType.BROWSER_NAME, browser);
+
+            driver = new RemoteWebDriver
+                    (new URL("http://"+seleniumHub+":"+seleniumPort+"/wd/hub"),chromeCaps);
+        } else if (browser.equals("edge")){
+            DesiredCapabilities edgeCaps = DesiredCapabilities.edge();
+            edgeCaps.setCapability(CapabilityType.BROWSER_NAME, browser);
+
+            driver = new RemoteWebDriver
+                    (new URL("http://"+seleniumHub+":"+seleniumPort+"/wd/hub"),edgeCaps);
+        }
         System.setProperty("webdriver.chrome.driver", "c:\\pliki\\chromedriver.exe");
 //        System.setProperty("webdriver.gecko.driver", "c:\\pliki\\geckodriver.exe");
 //        System.setProperty("webdriver.ie.driver", "c:\\pliki\\IEDriverServer.exe");
 //        System.setProperty("webdriver.edge.driver", "c:\\pliki\\MicrosoftWebDriver.exe");
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
 //        driver = new FirefoxDriver();
 //        driver = new InternetExplorerDriver();
 //        driver = new EdgeDriver();
-driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        DesiredCapabilities chromeCaps = DesiredCapabilities.chrome();
+//        chromeCaps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+//        chromeCaps.setCapability(CapabilityType.VERSION, "62.w10");
+//        chromeCaps.setCapability(CapabilityType.PLATFORM, "WIN10");
+//
+//        driver = new RemoteWebDriver(new URL("http://212.106.131.204:4443/wd/hub"),chromeCaps);
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.navigate().to("http://helion.pl");
 
-         }
+    }
 
     @AfterMethod
     public void tesrDown() {
